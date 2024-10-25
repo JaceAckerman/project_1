d8036db0135fb096ba10a9ef8d32bfd18eade463
JaceAckerman <105801652+JaceAckerman@users.noreply.github.com>
2024-10-25 16:54:48 -0500

    Update parser.py

--- project2_webScrapper/parser.py
+++ project2_webScrapper/parser.py
@@ -2,7 +2,7 @@
 # Date: 10/24/24
 # File: parser.py
 # Description: This file contains the Parser class. Parser utilizes the BeautifulSoap package. This package
-# how the response from Scraper gets processed. The setSoup() allows for the Parser's soup variable to be
+#  is how the response from Scraper gets processed. The setSoup() allows for the Parser's soup variable to be
 # changed. The getReviews() uses find_all() to extract all the review blocks and find() to get the reviews
 # themselves. Some reviews only had a title, so if this is the case the title is then used as the review.
 # All of the reviews are stored in the reviews array.
@@ -13,7 +13,7 @@ class Parser:
     
     #Parser has two variables, soup and a review array.
     def __init__(self):
-        #takes the Response object and puts it in soup so its in a structured format.
+        #takes the Response object and puts it in soup so it's in a structured format.
         self.soup = "None"
         self.reviews = []
     
@@ -26,7 +26,7 @@ class Parser:
         #gets all of the review bodies in the html document
         reviewsFromHTML = self.soup.find_all('div', class_='ebay-review-section-r') 
         
-        #gets each review from that people left for a product on eBay
+        #gets each review from the people who left a review for a product on eBay
         for review in reviewsFromHTML:
             # Get the review text, if no review then uses the title as the review.
             if review.find('p', class_='review-item-content') == None:
