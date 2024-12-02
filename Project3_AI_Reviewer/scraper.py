c0170686543ddd31752d129048e395ef5584e5c1
jace ackerman <jaceackerman7@gmail.com>
2024-12-02 11:35:17 -0600

    adding web scrapingcode

---
+++ Project3_AI_Reviewer/scraper.py
@@ -0,0 +1,30 @@
+# Author: Jace Ackerman
+# Date: 10/24/24
+# File: scraper.py
+# Description: This file contains the Scraper class. Scraper has a variable called url, which represents
+# the current url that the user wants to scrape from. Scraper has a setURL() which changes the url variable to 
+# the passed string in setURL(). Scraper utilizes the request package. It makes a GET request and if there is
+# an error, it will be caught. Otherwise, scrape() returns the response from the GET request in the form of 
+# text.
+
+import requests
+
+class Scraper:
+    
+    def __init__(self):
+        self.url = "Not Assigned Yet"
+     
+    #Retruns the value of the respsone from the get method from the requests library if valid
+    #otherwise it raises an exception    
+    def scrape(self):
+        try:
+            scrapedData = requests.get(self.url)
+            scrapedData.raise_for_status()
+            #return the data as text
+            return scrapedData.text
+        except requests.RequestException as e:
+            print(e)
+            return None
+    
+    def setURL(self, url):
+        self.url = url
